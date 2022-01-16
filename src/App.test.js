865cdf587a804724454127ffc09aafa1fd85e87d
Daniel Gutin <dgutin@vmware.com>
2022-01-16 13:35:04 +0200

    app test change

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ import App from './App';
 describe('Tests for [App]', () => {
 
   afterEach(cleanup);
-  it('Should render properly', () => {
+  it('Should render App properly', () => {
     render(<App />);
     const text = 'Let Granny locate books for you, tell me what book exactly you are looking for';
     const descriptionEl = screen.getByText(text);
